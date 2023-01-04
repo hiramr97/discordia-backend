3330493602bc1ab31e4dc120a6894b54709d7897
hiramr97 <hiram.rodriguez97@icloud.com>
2023-01-04 11:08:14 -0500

    added message urls

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from .views import ServerViewSet, ChannelViewSet
+from .views import ServerViewSet, ChannelViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register('servers', ServerViewSet, 'servers')
 router.register('channels', ChannelViewSet, 'channels')
+router.register('messages', MessageViewSet, 'messages')
 
 urlpatterns = router.urls
