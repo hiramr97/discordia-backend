30fba6cd6c3b72fe28a181d38a4df464298e61ee
hiramr97 <hiram.rodriguez97@icloud.com>
2023-01-04 10:53:11 -0500

    fixed channel viewset

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,8 @@
 from rest_framework import routers
-from .views import ServerViewSet
+from .views import ServerViewSet, ChannelViewSet
 
 router = routers.DefaultRouter()
 router.register('servers', ServerViewSet, 'servers')
-router.register('servers', ServerViewSet, 'servers')
+router.register('channels', ServerViewSet, 'channels')
 
 urlpatterns = router.urls
